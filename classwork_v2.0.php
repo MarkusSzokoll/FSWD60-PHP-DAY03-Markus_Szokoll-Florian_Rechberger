49de552ac2ab47d14957f9e8b7337446111be5f0
Florian Rechberger <florianrechberger@gmx.at>
2019-03-13 10:51:59 +0100

    add classwork_v2.0.php first draft

---
+++ classwork_v2.0.php
@@ -0,0 +1,44 @@
+<!DOCTYPE html>
+<html lang="en">
+  <head>
+    <meta charset="UTF-8">
+  <title>Add Record to User Table Form</title>
+  </head>
+  <body>
+  <?php
+    class Cigarettes
+    {
+      public $name;
+      public $package_size;
+      public $tar_amt;
+      public $nicotine_amt;
+
+      function __construct($name, $package_size, $tar_amt, $nicotine_amt)
+      {
+        $this->name= $name;
+        $this->package_size = $package_size;
+        $this->tar_amt = $tar_amt;
+        $this->nicotine_amt = $nicotine_amt;
+      }
+    }
+    $my_cigarettes[0] = new Cigarettes("Memphis", "20", "10", "0.8");
+    $my_cigarettes[1] = new Cigarettes("Happy Monkey", 20, 8, 0.7);
+    $my_cigarettes[2] = new Cigarettes("Hard Breathing", 19, 15, 0.5);
+    $my_cigarettes[3] = new Cigarettes("Lucky Strike", 19, 6, 0.2);
+    $my_cigarettes[4] = new Cigarettes("Cameleon", 21, 9, 0.6);
+
+    foreach ($my_cigarettes as $row) {
+      echo "<br>";
+      echo $row->name;
+      echo "<br>";
+      echo $row->package_size;
+      echo "<br>";
+      echo $row->tar_amt;
+      echo "<br>";
+      echo $row->nicotine_amt;
+      echo "<hr>";
+      }
+
+  ?>
+  </body>
+</html>
