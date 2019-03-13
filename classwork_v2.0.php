6e349c43f189137d207218213d7ea8e2a879d588
Markus Szokoll <markus.szokoll@gmx.at>
2019-03-13 11:24:00 +0100

    class cigarettes with functions and output

--- classwork_v2.0.php
+++ classwork_v2.0.php
@@ -13,21 +13,34 @@
       public $tar_amt;
       public $nicotine_amt;
 
-      function __construct($name, $package_size, $tar_amt, $nicotine_amt)
-      {
-        $this->name= $name;
-        $this->package_size = $package_size;
-        $this->tar_amt = $tar_amt;
-        $this->nicotine_amt = $nicotine_amt;
-      }
+	      function __construct($name, $package_size, $tar_amt, $nicotine_amt)
+	      {
+	        $this->name= $name;
+	        $this->package_size = $package_size;
+	        $this->tar_amt = $tar_amt;
+	        $this->nicotine_amt = $nicotine_amt;
+	      }
+
+		  public function showDetails() {
+		      return 'The cigarette brand is  ' . $this->name .
+		      ' and has ' . $this->package_size . ' cigarettes in a package' .
+		      ', it includes ' . $this->tar_amt . ' mg of tar and '  .
+		      $this->nicotine_amt . ' mg of nicotine. ';
+	      }
+
+
     }
     $my_cigarettes[0] = new Cigarettes("Memphis", "20", "10", "0.8");
     $my_cigarettes[1] = new Cigarettes("Happy Monkey", 20, 8, 0.7);
     $my_cigarettes[2] = new Cigarettes("Hard Breathing", 19, 15, 0.5);
     $my_cigarettes[3] = new Cigarettes("Lucky Strike", 19, 6, 0.2);
     $my_cigarettes[4] = new Cigarettes("Cameleon", 21, 9, 0.6);
+    
 
     foreach ($my_cigarettes as $row) {
+    	echo '<p>' . $row->showDetails() . '</p>';
+    }
+    	/*
       echo "<br>";
       echo $row->name;
       echo "<br>";
@@ -37,7 +50,8 @@
       echo "<br>";
       echo $row->nicotine_amt;
       echo "<hr>";
-      }
+      */
+      
 
   ?>
   </body>
